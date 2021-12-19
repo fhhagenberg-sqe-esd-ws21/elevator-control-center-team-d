760ba69b7b769d3c39a4fc84aabce541a4491731
LDrack <39444912+LDrack@users.noreply.github.com>
2021-12-19 11:25:45 +0100

    Implemented RMI Wrapper

---
+++ src/main/java/sqelevator/IRmiWrapper.java
@@ -0,0 +1,173 @@
+package sqelevator;
+
+import sqelevator.Elevator.ElevatorDirection;
+import sqelevator.Elevator.ElevatorDoorStatus;
+import java.rmi.RemoteException;
+
+/**
+ * Wraps IElevator. Checks indices for elevatorNumber and floorNumber.
+ * Uses enums for Elevator status instead of ints.
+ * @author Lucas Drack S2010567003
+ */
+public interface IRmiWrapper {	
+	
+	/**
+	 * Retrieves the committed direction of the specified elevator (up / down / uncommitted). 
+	 * @param elevatorNumber - elevator number whose committed direction is being retrieved 
+	 * @return the current direction of the specified elevator
+	 * @throws RemoteException when shit fails
+	 */
+	public ElevatorDirection getCommittedDirection(int elevatorNumber) throws RemoteException; 
+
+	/**
+	 * Provides the current acceleration of the specified elevator in feet per sec^2. 
+	 * @param elevatorNumber - elevator number whose acceleration is being retrieved 
+	 * @return returns the acceleration of the indicated elevator where positive speed is acceleration and negative is deceleration
+	 * @throws RemoteException when shit fails
+	 */
+	public int getElevatorAccel(int elevatorNumber) throws RemoteException;
+
+	/**
+	 * Provides the status of a floor request button on a specified elevator (on/off).      
+	 * @param elevatorNumber - elevator number whose button status is being retrieved
+	 * @param floor - floor number button being checked on the selected elevator 
+	 * @return returns boolean to indicate if floor button on the elevator is active (true) or not (false)
+	 * @throws RemoteException when shit fails
+	 */
+	public boolean getElevatorButton(int elevatorNumber, int floor) throws RemoteException;
+
+	/**
+	 * Provides the current status of the doors of the specified elevator (open/closed).      
+	 * @param elevatorNumber - elevator number whose door status is being retrieved 
+	 * @return returns the door status of the indicated elevator
+	 * @throws RemoteException when shit fails
+	 */
+	public ElevatorDoorStatus getElevatorDoorStatus(int elevatorNumber) throws RemoteException; 
+
+	/**
+	 * Provides the current location of the specified elevator to the nearest floor 
+	 * @param elevatorNumber - elevator number whose location is being retrieved 
+	 * @return returns the floor number of the floor closest to the indicated elevator
+	 * @throws RemoteException when shit fails
+	 */
+	public int getElevatorFloor(int elevatorNumber) throws RemoteException; 
+
+	/**
+	 * Retrieves the number of elevators in the building. 
+	 * @return total number of elevators
+	 * @throws RemoteException when shit fails
+	 */
+	public int getElevatorNum() throws RemoteException; 
+
+	/**
+	 * Provides the current location of the specified elevator in feet from the bottom of the building. 
+	 * @param elevatorNumber  - elevator number whose location is being retrieved 
+	 * @return returns the location in feet of the indicated elevator from the bottom of the building
+	 * @throws RemoteException when shit fails
+	 */
+	public int getElevatorPosition(int elevatorNumber) throws RemoteException; 
+
+	/**
+	 * Provides the current speed of the specified elevator in feet per sec. 
+	 * @param elevatorNumber - elevator number whose speed is being retrieved 
+	 * @return returns the speed of the indicated elevator where positive speed is up and negative is down
+	 * @throws RemoteException when shit fails
+	 */
+	public int getElevatorSpeed(int elevatorNumber) throws RemoteException; 
+
+	/**
+	 * Retrieves the weight of passengers on the specified elevator. 
+	 * @param elevatorNumber - elevator number whose service is being retrieved
+	 * @return total weight of all passengers in lbs
+	 * @throws RemoteException when shit fails
+	 */
+	public int getElevatorWeight(int elevatorNumber) throws RemoteException; 
+
+	/**
+	 * Retrieves the maximum number of passengers that can fit on the specified elevator.
+	 * @param elevatorNumber - elevator number whose service is being retrieved
+	 * @return number of passengers
+	 * @throws RemoteException when shit fails
+	 */
+	public int getElevatorCapacity(int elevatorNumber) throws RemoteException;
+	
+	/**
+	 * Provides the status of the Down button on specified floor (on/off). 
+	 * @param floor - floor number whose Down button status is being retrieved 
+	 * @return returns boolean to indicate if button is active (true) or not (false)
+	 * @throws RemoteException when shit fails
+	 */
+	public boolean getFloorButtonDown(int floor) throws RemoteException; 
+
+	/**
+	 * Provides the status of the Up button on specified floor (on/off). 
+	 * @param floor - floor number whose Up button status is being retrieved 
+	 * @return returns boolean to indicate if button is active (true) or not (false)
+	 * @throws RemoteException when shit fails
+	 */
+	public boolean getFloorButtonUp(int floor) throws RemoteException; 
+
+	/**
+	 * Retrieves the height of the floors in the building. 
+	 * @return floor height (ft)
+	 * @throws RemoteException when shit fails
+	 */
+	public int getFloorHeight() throws RemoteException; 
+
+	/**
+	 * Retrieves the number of floors in the building. 
+	 * @return total number of floors
+	 * @throws RemoteException when shit fails
+	 */
+	public int getFloorNum() throws RemoteException; 
+
+	/** 
+	 * Retrieves whether or not the specified elevator will service the specified floor (yes/no). 
+	 * @param elevatorNumber elevator number whose service is being retrieved
+	 * @param floor floor whose service status by the specified elevator is being retrieved
+	 * @return service status whether the floor is serviced by the specified elevator (yes=true,no=false)
+	 * @throws RemoteException when shit fails
+	 */
+	public boolean getServicesFloors(int elevatorNumber, int floor) throws RemoteException; 
+
+	/**
+	 * Retrieves the floor target of the specified elevator. 
+	 * @param elevatorNumber elevator number whose target floor is being retrieved
+	 * @return current floor target of the specified elevator
+	 * @throws RemoteException when shit fails
+	 */
+	public int getTarget(int elevatorNumber) throws RemoteException; 
+
+	/**
+	 * Sets the committed direction of the specified elevator (up / down / uncommitted). 
+	 * @param elevatorNumber elevator number whose committed direction is being set
+	 * @param direction direction being set where up=0, down=1 and uncommitted=2
+	 * @throws RemoteException when shit fails
+	 */
+	public void setCommittedDirection(int elevatorNumber, ElevatorDirection direction) throws RemoteException;
+
+	/**
+	 * Sets whether or not the specified elevator will service the specified floor (yes/no). 
+	 * @param elevatorNumber elevator number whose service is being defined
+	 * @param floor floor whose service by the specified elevator is being set
+	 * @param service indicates whether the floor is serviced by the specified elevator (yes=true,no=false)
+	 * @throws RemoteException when shit fails
+	 */
+	public void setServicesFloors(int elevatorNumber, int floor, boolean service) throws RemoteException; 
+
+	/**
+	 * Sets the floor target of the specified elevator. 
+	 * @param elevatorNumber elevator number whose target floor is being set
+	 * @param target floor number which the specified elevator is to target
+	 * @throws RemoteException when shit fails
+	 */
+	public void setTarget(int elevatorNumber, int target) throws RemoteException; 
+
+	/**
+	 * Retrieves the current clock tick of the elevator control system. 
+	 * @return clock tick
+	 * @throws RemoteException when shit fails
+	 */
+	public long getClockTick() throws RemoteException;
+	
+}
---
+++ src/main/java/sqelevator/RmiWrapper.java
@@ -0,0 +1,176 @@
+package sqelevator;
+import sqelevator.Elevator.ElevatorDirection;
+import sqelevator.Elevator.ElevatorDoorStatus;
+import java.rmi.RemoteException;
+
+public class RmiWrapper implements IRmiWrapper{
+
+	private final IElevator mRmiInterface;
+	private final int mNrOfElevators;
+	private final int mNrOfFloors;
+	
+	public RmiWrapper(IElevator rmiInterface) throws RemoteException {
+		if (rmiInterface == null) {
+			throw new IllegalArgumentException("IElevator must not be null");
+		}
+		
+		mRmiInterface = rmiInterface;
+		mNrOfElevators = mRmiInterface.getElevatorNum();
+		mNrOfFloors = mRmiInterface.getFloorNum();
+	}
+
+	private void checkElevatorNumber(int elevatorNumber) {
+		if (elevatorNumber >= mNrOfElevators || elevatorNumber < 0) {
+			throw new IllegalArgumentException("Invalid elevator number " + elevatorNumber);
+		}
+	}
+	
+	private void checkFloorNumber(int floor) {
+		if (floor >= mNrOfFloors || floor < 0) { 
+			throw new IllegalArgumentException("Invalid floor number " + floor);
+		}
+	}
+	
+	@Override
+	public ElevatorDirection getCommittedDirection(int elevatorNumber) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		
+		int direction = mRmiInterface.getCommittedDirection(elevatorNumber);
+		return switch (direction) {
+			case IElevator.ELEVATOR_DIRECTION_UP -> ElevatorDirection.Up;
+			case IElevator.ELEVATOR_DIRECTION_DOWN -> ElevatorDirection.Down;
+			case IElevator.ELEVATOR_DIRECTION_UNCOMMITTED -> ElevatorDirection.Uncommitted;
+			default -> throw new IllegalArgumentException("Invalid Elevator direction");
+		};
+	}
+
+	@Override
+	public int getElevatorAccel(int elevatorNumber) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		return mRmiInterface.getElevatorAccel(elevatorNumber);
+	}
+
+	@Override
+	public boolean getElevatorButton(int elevatorNumber, int floor) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		checkFloorNumber(floor);
+		return mRmiInterface.getElevatorButton(elevatorNumber, floor);
+	}
+
+	@Override
+	public ElevatorDoorStatus getElevatorDoorStatus(int elevatorNumber) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		
+		int door = mRmiInterface.getElevatorDoorStatus(elevatorNumber);
+		return switch (door) {
+			case IElevator.ELEVATOR_DOORS_OPEN -> ElevatorDoorStatus.Open;
+			case IElevator.ELEVATOR_DOORS_CLOSED -> ElevatorDoorStatus.Closed;
+			case IElevator.ELEVATOR_DOORS_OPENING -> ElevatorDoorStatus.Opening;
+			case IElevator.ELEVATOR_DOORS_CLOSING -> ElevatorDoorStatus.Closing;
+			default -> throw new IllegalArgumentException("Invalid Elevator door status");
+		};
+	}
+
+	@Override
+	public int getElevatorFloor(int elevatorNumber) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		return mRmiInterface.getElevatorFloor(elevatorNumber);
+	}
+
+	@Override
+	public int getElevatorNum() {
+		return mNrOfElevators;
+	}
+
+	@Override
+	public int getElevatorPosition(int elevatorNumber) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		return mRmiInterface.getElevatorPosition(elevatorNumber);
+	}
+
+	@Override
+	public int getElevatorSpeed(int elevatorNumber) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		return mRmiInterface.getElevatorSpeed(elevatorNumber);
+	}
+
+	@Override
+	public int getElevatorWeight(int elevatorNumber) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		return mRmiInterface.getElevatorWeight(elevatorNumber);
+	}
+
+	@Override
+	public int getElevatorCapacity(int elevatorNumber) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		return mRmiInterface.getElevatorCapacity(elevatorNumber);
+	}
+
+	@Override
+	public boolean getFloorButtonDown(int floor) throws RemoteException {
+		checkFloorNumber(floor);
+		return mRmiInterface.getFloorButtonDown(floor);
+	}
+
+	@Override
+	public boolean getFloorButtonUp(int floor) throws RemoteException {
+		checkFloorNumber(floor);
+		return mRmiInterface.getFloorButtonUp(floor);
+	}
+
+	@Override
+	public int getFloorHeight() throws RemoteException {
+		return mRmiInterface.getFloorHeight();
+	}
+
+	@Override
+	public int getFloorNum() {
+		return mNrOfFloors;
+	}
+
+	@Override
+	public boolean getServicesFloors(int elevatorNumber, int floor) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		checkFloorNumber(floor);
+		return mRmiInterface.getServicesFloors(elevatorNumber, floor);
+	}
+
+	@Override
+	public int getTarget(int elevatorNumber) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		return mRmiInterface.getTarget(elevatorNumber);
+	}
+
+	@Override
+	public void setCommittedDirection(int elevatorNumber, ElevatorDirection direction) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		
+		int d = switch (direction) {
+			case Up -> IElevator.ELEVATOR_DIRECTION_UP;
+			case Down -> IElevator.ELEVATOR_DIRECTION_DOWN;
+			case Uncommitted -> IElevator.ELEVATOR_DIRECTION_UNCOMMITTED;
+			default -> throw new IllegalArgumentException("Invalid ElevatorDirection");
+		};
+
+		mRmiInterface.setCommittedDirection(elevatorNumber, d);
+	}
+
+	@Override
+	public void setServicesFloors(int elevatorNumber, int floor, boolean service) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		checkFloorNumber(floor);
+		mRmiInterface.setServicesFloors(elevatorNumber, floor, service);
+	}
+
+	@Override
+	public void setTarget(int elevatorNumber, int targetFloor) throws RemoteException {
+		checkElevatorNumber(elevatorNumber);
+		checkFloorNumber(targetFloor);
+		mRmiInterface.setTarget(elevatorNumber, targetFloor);
+	}
+
+	@Override
+	public long getClockTick() throws RemoteException {
+		return mRmiInterface.getClockTick();
+	}
+}
