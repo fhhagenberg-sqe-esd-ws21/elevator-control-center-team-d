681f38d180cd00e377604e98a7f4a16d0ec7607a
LDrack <39444912+LDrack@users.noreply.github.com>
2022-01-10 18:31:56 +0100

    ECC first implementation - RMI lookup and model instantiation

---
+++ src/main/java/sqelevator/ECC.java
@@ -0,0 +1,51 @@
+package sqelevator;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class ECC {
+    private ECCDataModel model;
+    private ECCUpdater updater;
+    private RmiWrapper wrapper;
+    private boolean connected = false;
+
+    public void init() {
+        try {
+            connect();
+            model = new ECCFactory(wrapper).createElevatorControlCenter();
+            updater = new ECCUpdater(wrapper, model);
+            updater.updateModel();
+        } 
+        catch (NotBoundException e) {
+            System.err.println("Remote server not reachable. " + e.getMessage());
+        }
+        catch (MalformedURLException e) {
+            System.err.println("Invalid URL: " + e.getMessage());
+        }
+        catch (RemoteException e) {
+            System.err.println("Remote exception on connecting: " + e.getMessage());
+        }
+    }
+
+    // RMI lookup
+    private void connect() throws MalformedURLException, RemoteException, NotBoundException {
+        connected = false;
+        IElevator rmiInterface = (IElevator)Naming.lookup("rmi://localhost/ElevatorSim");
+        wrapper = new RmiWrapper(rmiInterface);
+        connected = true;
+    }
+
+    // Call this in an interval by using a timer or something
+    public void update() {
+        if (connected) {
+            updater.updateModel();
+        }
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+}
