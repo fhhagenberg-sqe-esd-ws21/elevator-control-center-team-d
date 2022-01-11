adea36d8dad832fea2fe307d285b2aebb13e9b55
LDrack <39444912+LDrack@users.noreply.github.com>
2022-01-11 19:16:36 +0100

    Added send-Functions to mocked IElevator -> Communication from Test to mocked Interface with set-Commands

--- src/test/java/at/fhhagenberg/sqe/MockedIElevator.java
+++ src/test/java/at/fhhagenberg/sqe/MockedIElevator.java
@@ -6,6 +6,11 @@ import org.mockito.Mockito;
 import sqelevator.IElevator;
 import java.rmi.RemoteException;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
 
 /**
@@ -60,6 +65,50 @@ public class MockedIElevator {
         when(mock.getFloorHeight()).thenReturn(height);
     }
 
+
+    public void sendCommittedDirection() throws RemoteException {
+        doAnswer(invocation -> {
+            // Fetch arguments of call to setCommitedDirection
+            Integer elevatorNumber = invocation.getArgument(0);
+            Integer direction = invocation.getArgument(1);
+            
+            // Validate that arguments are legal
+            assertTrue(elevatorNumber >= 0 && elevatorNumber < mock.getElevatorNum());
+            assertTrue(direction >= IElevator.ELEVATOR_DIRECTION_UP && 
+            direction <= IElevator.ELEVATOR_DIRECTION_UNCOMMITTED);
+
+            // Change future mock behaviour
+            when(mock.getCommittedDirection(elevatorNumber)).thenReturn(direction);
+            return null;
+        }).when(mock).setCommittedDirection(anyInt(), anyInt());
+    }
+
+    public void sendServicesFloors() throws RemoteException {
+        doAnswer(invocation -> {
+            Integer elevatorNr = invocation.getArgument(0);
+            Integer floorNr = invocation.getArgument(1);
+            Boolean services = invocation.getArgument(2);
+            
+            assertTrue(elevatorNr >= 0 && elevatorNr < mock.getElevatorNum());
+            assertTrue(floorNr >= 0 && floorNr < mock.getFloorNum());
+
+            when(mock.getServicesFloors(elevatorNr, floorNr)).thenReturn(services);
+            return null;
+        }).when(mock).setServicesFloors(anyInt(), anyInt(), anyBoolean());
+    }
+
+    public void sendTarget() throws RemoteException {
+        doAnswer(invocation -> {
+            Integer elevatorNr = invocation.getArgument(0);
+            Integer target = invocation.getArgument(1);
+            
+            assertTrue(elevatorNr >= 0 && elevatorNr < mock.getElevatorNum());
+            assertTrue(target >= 0 && target < mock.getElevatorNum());
+
+            when(mock.getTarget(elevatorNr)).thenReturn(target);
+            return null;
+        }).when(mock).setTarget(anyInt(), anyInt());
+    }
 
 
 
