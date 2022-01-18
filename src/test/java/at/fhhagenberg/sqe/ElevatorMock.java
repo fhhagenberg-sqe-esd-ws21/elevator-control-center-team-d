0d7f77c5ebd234c7a69ab1a796b01ac11a6f6bf8
LDrack <39444912+LDrack@users.noreply.github.com>
2022-01-18 17:59:38 +0100

    Finished ElevatorMock

--- src/test/java/at/fhhagenberg/sqe/ElevatorMock.java
+++ src/test/java/at/fhhagenberg/sqe/ElevatorMock.java
@@ -9,18 +9,71 @@ import sqelevator.IElevator;
 
 public class ElevatorMock implements IElevator {
 
-    private int commitedDirection[];
-    private int targets[];
-    private boolean servicedFloors[][];
-    private int floorNum = 0;
-    private int elevatorNum = 0;
+    private final int floorNum;
+    private final int elevatorNum;
+    private final int floorHeight;
 
-    public ElevatorMock(int nElevators, int nFloors) {
-        commitedDirection = new int[nElevators];
-        targets = new int[nElevators];
-        servicedFloors = new boolean[nElevators][nFloors];
+    private int commitedDirection[];
+    private int doorStatus[];
+    private int targets[];
+    private boolean stopButtons[][];
+    private boolean servicedFloors[][];
+    private int weights[];
+    private int accelerations[];
+    private int positions[];
+    private int floors[];
+    private int speeds[];
+    private int capacities[];
+
+    private boolean floorButtonsDown[];
+    private boolean floorButtonsUp[];
+
+
+    public ElevatorMock(int nElevators, int nFloors, int floorHeightFt) {
+        assertTrue(nElevators >= 0);
+        assertTrue(nFloors >= 0);
+        assertTrue(floorHeightFt >= 0);
+
         floorNum = nFloors;
         elevatorNum = nElevators;
+        floorHeight = floorHeightFt;
+
+        stopButtons = new boolean[nElevators][nFloors];
+        servicedFloors = new boolean[nElevators][nFloors];
+        commitedDirection = new int[nElevators];
+        doorStatus = new int[nElevators];
+        targets = new int[nElevators];
+        weights = new int[nElevators];
+        accelerations = new int[nElevators];
+        positions = new int[nElevators];
+        floors = new int[nElevators];
+        speeds = new int[nElevators];
+        capacities = new int[nElevators];
+
+        floorButtonsDown = new boolean[nFloors];
+        floorButtonsUp = new boolean[nFloors];
+
+        for (int i = 0; i < nElevators; i++) {
+            commitedDirection[i] = 0;
+            doorStatus[i] = 0;
+            targets[i] = 0;
+            weights[i] = 0;
+            accelerations[i] = 0;
+            positions[i] = 0;
+            floors[i] = 0;
+            speeds[i] = 0;
+            capacities[i] = 0;
+
+            for (int f = 0; f < nFloors; f++) {
+                stopButtons[i][f] = false;
+                servicedFloors[i][f] = false;
+            }
+        }
+
+        for (int f = 0; f < nFloors; f++) {
+            floorButtonsDown[f] = false;
+            floorButtonsUp[f] = false;
+        }
     }
 
     @Override
@@ -30,9 +83,15 @@ public class ElevatorMock implements IElevator {
     }
 
     @Override
+    public int getElevatorDoorStatus(int elevatorNumber) throws RemoteException {
+        assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
+        return doorStatus[elevatorNumber];
+    }
+
+    @Override
     public int getElevatorAccel(int elevatorNumber) throws RemoteException {
         assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
-        return 3;
+        return accelerations[elevatorNumber];
     }
 
     @Override
@@ -40,20 +99,13 @@ public class ElevatorMock implements IElevator {
         assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
         assertTrue(floor >= 0 && floor < floorNum);
 
-        return true;
-    }
-
-    @Override
-    public int getElevatorDoorStatus(int elevatorNumber) throws RemoteException {
-        assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
-        return 1; // closed
+        return stopButtons[elevatorNumber][floor];
     }
 
     @Override
     public int getElevatorFloor(int elevatorNumber) throws RemoteException {
         assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
-
-        return 0;
+        return floors[elevatorNumber];
     }
 
     @Override
@@ -64,42 +116,42 @@ public class ElevatorMock implements IElevator {
     @Override
     public int getElevatorPosition(int elevatorNumber) throws RemoteException {
         assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
-        return 0;
+        return positions[elevatorNumber];
     }
 
     @Override
     public int getElevatorSpeed(int elevatorNumber) throws RemoteException {
         assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
-        return 3;
+        return speeds[elevatorNumber];
     }
 
     @Override
     public int getElevatorWeight(int elevatorNumber) throws RemoteException {
         assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
-        return 100;
+        return weights[elevatorNumber];
     }
 
     @Override
     public int getElevatorCapacity(int elevatorNumber) throws RemoteException {
         assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
-        return 10;
+        return capacities[elevatorNumber];
     }
 
     @Override
     public boolean getFloorButtonDown(int floor) throws RemoteException {
         assertTrue(floor >= 0 && floor < floorNum);
-        return false;
+        return floorButtonsDown[floor];
     }
 
     @Override
     public boolean getFloorButtonUp(int floor) throws RemoteException {
         assertTrue(floor >= 0 && floor < floorNum);
-        return false;
+        return floorButtonsUp[floor];
     }
 
     @Override
     public int getFloorHeight() throws RemoteException {
-        return 2;
+        return floorHeight;
     }
 
     @Override
@@ -122,9 +174,23 @@ public class ElevatorMock implements IElevator {
         return targets[elevatorNumber];
     }
 
+
+
+
+
+
+    @Override
+    public void setTarget(int elevatorNumber, int target) throws RemoteException {
+        assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
+        assertTrue(target >= 0 && target < floorNum);
+
+        targets[elevatorNumber] = target;
+    }
+
     @Override
     public void setCommittedDirection(int elevatorNumber, int direction) throws RemoteException {
         assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
+        assertTrue(direction >= IElevator.ELEVATOR_DIRECTION_UP && direction <= IElevator.ELEVATOR_DIRECTION_UNCOMMITTED);
 
         commitedDirection[elevatorNumber] = direction;
     }
@@ -135,21 +201,73 @@ public class ElevatorMock implements IElevator {
         assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
 
         servicedFloors[elevatorNumber][floor] = service;
-
     }
-
-    @Override
-    public void setTarget(int elevatorNumber, int target) throws RemoteException {
+    
+    public void setDoorStatus(int elevatorNumber, int status) throws RemoteException {
         assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
-        assertTrue(target >= 0 && target < floorNum);
+        assertTrue(status >= IElevator.ELEVATOR_DOORS_OPEN && status <= IElevator.ELEVATOR_DOORS_CLOSING);
 
-        targets[elevatorNumber] = target;
-
+        commitedDirection[elevatorNumber] = status;
     }
+
+    
+
+
+
+    public void setElevatorAccel(int elevatorNumber, int acc) throws RemoteException {
+        assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
+        accelerations[elevatorNumber] = acc;
+    }
+
+    public void setElevatorButton(int elevatorNumber, int floor, boolean button) throws RemoteException {
+        assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
+        assertTrue(floor >= 0 && floor < floorNum);
+
+        stopButtons[elevatorNumber][floor] = button;
+    }
+
+    public void setElevatorFloor(int elevatorNumber, int floor) throws RemoteException {
+        assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
+        floors[elevatorNumber] = floor;
+    }
+
+    public void setElevatorPosition(int elevatorNumber, int pos) throws RemoteException {
+        assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
+        positions[elevatorNumber] = pos;
+    }
+
+    public void setElevatorSpeed(int elevatorNumber, int speed) throws RemoteException {
+        assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
+        speeds[elevatorNumber] = speed;
+    }
+
+    public void setElevatorWeight(int elevatorNumber, int weight) throws RemoteException {
+        assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
+        weights[elevatorNumber] = weight;
+    }
+
+    public void setElevatorCapacity(int elevatorNumber, int cap) throws RemoteException {
+        assertTrue(elevatorNumber >= 0 && elevatorNumber < elevatorNum);
+        capacities[elevatorNumber] = cap;
+    }
+
+    public void setFloorButtonDown(int floor, boolean val) throws RemoteException {
+        assertTrue(floor >= 0 && floor < floorNum);
+        floorButtonsDown[floor] = val;
+    }
+
+    public void setFloorButtonUp(int floor, boolean val) throws RemoteException {
+        assertTrue(floor >= 0 && floor < floorNum);
+        floorButtonsUp[floor] = val;
+    }
+
+
+
+
+
 
     @Override
     public long getClockTick() throws RemoteException {
-        // TODO Auto-generated method stub
         return 0;
     }
 
